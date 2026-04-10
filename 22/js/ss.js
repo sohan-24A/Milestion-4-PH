6973622a05aac98666c6e182645901843546b0e7
sohan24-dev <sohan.explorer@gmail.com>
2026-04-10 16:51:14 +0600

    Add

---
+++ 22/js/ss.js
@@ -0,0 +1,19 @@
+// console.log("5" - 2);
+// console.log("5" + 2);
+// console.log( 2 + '5');
+// console.log(2 - "5");
+
+
+
+function multiplyByTwo(n, cb) {
+  cb(n * 2);
+//   const result = cd + 
+//   console.log(object);
+}
+
+multiplyByTwo(10,addFive)
+function addFive(n, cb) {
+  cb(n + 5);
+
+}
+// addFive(10,multiplyByTwo)
